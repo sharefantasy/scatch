6c92f2832a3ff37a3da6626504a7b51c1da0f338
fanqineng <fanqineng@bytedance.com>
2023-01-29 16:01:50 +0800

    simple adding
    Change-Id: I8dd8da97dd27a3172039e5a97cff0b144b486bab

--- init.lua
+++ init.lua
@@ -1,13 +1,18 @@
-return require('packer').startup(function(use) 
+return require('packer').startup(function(use)
+    -- lua
+    use 'folke/neodev.nvim'
+    -- fennel
     use 'Olical/nvim-local-fennel'
-    use { 'Olical/aniseed', run = function () 
-        vim.g["aniseed#env"] = { module = "dotfiles.init", compile = true } 
+    use { 'Olical/aniseed', config = function ()
+        vim.g["aniseed#env"] = { module = "dotfiles.init", compile = true }
     end }
-    use 'Olical/fennel.vim'
+
+    use {'Olical/fennel.vim', ft = 'fnl'}
     use 'Olical/conjure'
-    use 'tpope/vim-vinegar'
-    use {'Shougo/deoplete.nvim', cmd = 'UpdateRemotePlugins', run = function() 
-        vim.g["deoplete#enable_at_startup"] = 1 
+    use {'udayvir-singh/hibiscus.nvim', ft = 'fnl'}
+
+    use {'Shougo/deoplete.nvim', cmd = 'UpdateRemotePlugins', run = function()
+        vim.g["deoplete#enable_at_startup"] = 1
     end }
 
     use 'wbthomason/packer.nvim'
@@ -15,20 +20,26 @@ return require('packer').startup(function(use)
     use 'nvim-lua/plenary.nvim'
     use 'nvim-treesitter/nvim-treesitter'
     use 'tsbohc/zest.nvim'
+    use 'feline-nvim/feline.nvim'
 
+    -- ui
     use 'kyazdani42/nvim-web-devicons'
     use 'yamatsum/nvim-nonicons'
     use 'glepnir/zephyr-nvim'
     use 'norcalli/nvim-colorizer.lua'
-    use 'glepnir/galaxyline.nvim'
+    use {'ray-x/guihua.lua', run = 'cd lua/fzy && make'}
 
+    -- ux
     use 'francoiscabrol/ranger.vim'
     use 'kyazdani42/nvim-tree.lua'
     use 'folke/which-key.nvim'
-    use 'sharefantasy/transient'
+    use 'Nexmean/caskey.nvim'
     use 'rcarriga/nvim-notify'
     use 'romgrk/barbar.nvim'
+    use 'RRethy/vim-illuminate'
+    use {"shortcuts/no-neck-pain.nvim"}
 
+    -- editor
     use 'Olical/vim-enmasse'
     use 'PeterRincker/vim-argumentative'
     use 'jiangmiao/auto-pairs'
@@ -45,9 +56,31 @@ return require('packer').startup(function(use)
     use 'tpope/vim-commentary'
     use 'tpope/vim-eunuch'
     use 'tpope/vim-sleuth'
-    use 'justinmk/vim-sneak'
-    use 'norcalli/snippets.nvim'
+    use { "danymat/neogen", requires = "nvim-treesitter/nvim-treesitter", config = function()
+        require('neogen').setup {}
+    end }
+    use {'phaazon/hop.nvim', config = function ()
+        require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    end }
+    use {
+        "chrisgrieser/nvim-various-textobjs",
+        config = function ()
+            require("various-textobjs").setup({ useDefaultKeymaps = true })
+        end,
+    }
+    use { 'mhartington/formatter.nvim' }
 
+
+
+    -- snippet
+    use 'norcalli/snippets.nvim'
+    use 'rafamadriz/friendly-snippets'
+    use 'b0o/SchemaStore.nvim'
+    use({ "L3MON4D3/LuaSnip", run = "make install_jsregexp" })
+    use { "windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup {} end }
+
+
+    -- startup & ext
     use 'tweekmonster/startuptime.vim'
     use 'tpope/vim-dadbod'
     use 'tpope/vim-dispatch'
@@ -56,6 +89,10 @@ return require('packer').startup(function(use)
     use 'neomake/neomake'
     use 'glacambre/firenvim'
 
+
+    -- lsp management
+    use { "williamboman/mason.nvim" , config = function () require("mason").setup() end }
+    use { "williamboman/mason-lspconfig" , config = function () require("mason-lspconfig").setup() end }
     use 'neovim/nvim-lspconfig'
     use 'kabouzeid/nvim-lspinstall'
     use 'glepnir/lspsaga.nvim'
@@ -68,26 +105,60 @@ return require('packer').startup(function(use)
     use 'kosayoda/nvim-lightbulb'
     use 'folke/trouble.nvim'
 
+    -- git
     use 'lewis6991/gitsigns.nvim'
-    use 'sindrets/diffview.nvim'
-    use 'TimUntersberger/neogit'
+    use { 'sindrets/diffview.nvim', requires = {'nvim-lua/plenary.nvim'}}
+    use { 'TimUntersberger/neogit', requires = 'sindrets/diffview.nvim', config = function ()
+        require('neogit').setup({
+          integrations = {
+            diffview = true
+          }
+        })
+    end }
 
-    use 'vijaymarupudi/nvim-fzf'
+    -- telescope
     use 'nvim-telescope/telescope.nvim'
     use 'nvim-telescope/telescope-hop.nvim'
     use 'nvim-telescope/telescope-frecency.nvim'
     use 'nvim-telescope/telescope-project.nvim'
     use 'nvim-telescope/telescope-packer.nvim'
     use 'nvim-telescope/telescope-snippets.nvim'
+    use 'nvim-telescope/telescope-file-browser.nvim'
+    use { "LinArcX/telescope-command-palette.nvim" }
+    use 'nvim-lua/popup.nvim'
     use 'MattesGroeger/vim-bookmarks'
     use 'tom-anders/telescope-vim-bookmarks.nvim'
+    use 'vijaymarupudi/nvim-fzf'
 
-    use 'kristijanhusak/orgmode.nvim'
+    -- language support
+    use {'kristijanhusak/orgmode.nvim', ft = 'org'}
     use 'wlangstroth/vim-racket'
-    use 'ray-x/go.nvim'
+    use {'ray-x/go.nvim', ft = "go", config = function ()
+        local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          pattern = "*.go",
+          callback = function()
+           require('go.format').goimport()
+          end,
+          group = format_sync_grp,
+        })
+        require('go').setup()
+    end }
+
     use { "ellisonleao/gruvbox.nvim" , config = function () 
         vim.cmd.colorscheme ("gruvbox") 
     end }
 
+    -- markdown
+    use {"ellisonleao/glow.nvim", ft = 'md', config = function() require('glow').setup() end}
+
+
+    -- general development
+    use 'hkupty/iron.nvim'
+
+    -- performance
+    use {'lewis6991/impatient.nvim', config = function () 
+        require('impatient')
+    end }
 end)
 
